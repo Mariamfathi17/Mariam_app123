e43174f93d19965fbe2a993f7cdb70c3db90ee23
Mariamfathi17 <86616263+Mariamfathi17@users.noreply.github.com>
2024-08-26 20:48:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 import pickle 
-# Set up the Streamlit app
-st.set_page_config(page_title="Diabetes Prediction App", layout="centered")
-
-# Title and description
 st.title("Diabetes Prediction App")
 st.write("Fill in the details below to predict the likelihood of diabetes.")
 
